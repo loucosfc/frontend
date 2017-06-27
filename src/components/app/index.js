65ed686161c1c1722658e71e93be8eeb8081a69a
lhas <luizhrqas@gmail.com>
2017-06-26 21:16:51 -0300

    added logo

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -5,12 +5,24 @@ import {
 } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import * as colors from 'material-ui/styles/colors'
+import './app.css';
 
 export default () => (
     <BrowserRouter>
-      <div className="app">
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-      </div>
+      <MuiThemeProvider>
+        <div className="app">
+        <AppBar
+          titleStyle={{ color: colors.grey900 }}
+          style={{ background: colors.grey300, textAlign: 'center' }}
+          title={<img src="http://loucosfc.com/assets/logo-846f77c8d6ca20d7d2f6b6eb698c2aa6.png" height="50" style={{margin: 6}} />}
+          showMenuIconButton={false}
+        />
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+        </div>
+      </MuiThemeProvider>
     </BrowserRouter>
 );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './components/app';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
