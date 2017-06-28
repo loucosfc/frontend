92628ac34c9acb3896d9eba10ce6d58c504551df
lhas <luizhrqas@gmail.com>
2017-06-28 06:51:44 -0300

    now we have a maintenanceMode prop for Team

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -12,17 +12,47 @@ import { SocketProvider } from 'socket.io-react'
 
 const socket = require('socket.io-client')(process.env.REACT_APP_API_URL);
 
-export default () => (
-    <BrowserRouter>
-      <SocketProvider socket={socket}>
-        <MuiThemeProvider>
-          <div className="app">
-            <Route exact path="/" component={Home}/>
-            {teams.map((v) => (
-              <Route key={v.slug} path={`/${v.slug}`} component={Team}/>
-            ))}
-          </div>
-        </MuiThemeProvider>
-      </SocketProvider>
-    </BrowserRouter>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      maintenanceMode: true,
+    };
+  }
+
+  componentDidMount() {
+    socket.on('connect', () => {
+      this.setMaintenanceMode(false);
+    });
+
+    socket.on('disconnect', () => {
+      this.setMaintenanceMode(true);
+    });
+  }
+
+  setMaintenanceMode(value) {
+    console.log(value);
+    this.setState({
+      maintenanceMode: value,
+    });
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
+        <SocketProvider socket={socket}>
+          <MuiThemeProvider>
+            <div className="app">
+              <Route exact path="/" component={Home}/>
+              {teams.map((v) => (
+                <Route key={v.slug} path={`/${v.slug}`} component={() => <Team maintenanceMode={this.state.maintenanceMode} />} />
+              ))}
+            </div>
+          </MuiThemeProvider>
+        </SocketProvider>
+      </BrowserRouter>
+    );
+  }
+}
+export default App;
--- src/components/team/index.js
+++ src/components/team/index.js
@@ -7,35 +7,14 @@ import CircularProgress from 'material-ui/CircularProgress';
 import './team.css';
 
 class Team extends React.Component {
-  state = {
-    maintenanceMode: true,
-  };
-
-  componentDidMount() {
-    this.props.socket.on('connect', () => {
-      console.log('Cliente conectou!');
-      this.setMaintenanceMode(false);
-    });
-    this.props.socket.on('disconnect', () => {
-      this.setMaintenanceMode(true);
-      console.log('Cliente desconectou!');
-    });
-  }
-
-  setMaintenanceMode(value) {
-    this.setState({
-      maintenanceMode: value,
-    });
-  };
-
   render() {
     return (
       <Grid className="team">
         <Header history={this.props.history} />
-        {this.state.maintenanceMode &&
+        {this.props.maintenanceMode &&
           <div className="maintenance-mode">
             <div className="progress">
-              <CircularProgress color="#e7a33a" size="256" thickness="5" />
+              <CircularProgress color="#e7a33a" size={256} thickness={5} />
             </div>
           </div>
         }
