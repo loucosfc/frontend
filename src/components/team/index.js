a53dd7eb2efa67347ba1704c6c0290d37dfc61d9
lhas <luizhrqas@gmail.com>
2017-06-28 06:40:13 -0300

    added maintenance mode at teams page

--- src/components/team/index.js
+++ src/components/team/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import { Grid } from 'react-flexbox-grid';
 import Header from '../header';
+import { socketConnect } from 'socket.io-react';
+import CircularProgress from 'material-ui/CircularProgress';
+
+import './team.css';
 
 class Team extends React.Component {
+  state = {
+    maintenanceMode: true,
+  };
+
+  componentDidMount() {
+    this.props.socket.on('connect', () => {
+      console.log('Cliente conectou!');
+      this.setMaintenanceMode(false);
+    });
+    this.props.socket.on('disconnect', () => {
+      this.setMaintenanceMode(true);
+      console.log('Cliente desconectou!');
+    });
+  }
+
+  setMaintenanceMode(value) {
+    this.setState({
+      maintenanceMode: value,
+    });
+  };
+
   render() {
     return (
       <Grid className="team">
         <Header history={this.props.history} />
-        ae
+        {this.state.maintenanceMode &&
+          <div className="maintenance-mode">
+            <div className="progress">
+              <CircularProgress color="#e7a33a" size="256" thickness="5" />
+            </div>
+          </div>
+        }
       </Grid>
     )
   };
 }
 
-export default Team;
+export default socketConnect(Team);
 
