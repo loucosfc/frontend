df715081b0cfe36c7dabf4ce986d2292233963aa
lhas <luizhrqas@gmail.com>
2017-06-28 06:24:15 -0300

    added HOC for socket and favicon

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -7,17 +7,22 @@ import Home from '../home'
 import Team from '../team'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import './app.css';
-import teams from '../../utils/teams';
+import teams from '../../utils/teams'
+import { SocketProvider } from 'socket.io-react'
+
+const socket = require('socket.io-client')(process.env.REACT_APP_API_URL);
 
 export default () => (
     <BrowserRouter>
-      <MuiThemeProvider>
-        <div className="app">
-          <Route exact path="/" component={Home}/>
-          {teams.map((v) => (
-            <Route path={`/${v.slug}`} component={Team}/>
-          ))}
-        </div>
-      </MuiThemeProvider>
+      <SocketProvider socket={socket}>
+        <MuiThemeProvider>
+          <div className="app">
+            <Route exact path="/" component={Home}/>
+            {teams.map((v) => (
+              <Route key={v.slug} path={`/${v.slug}`} component={Team}/>
+            ))}
+          </div>
+        </MuiThemeProvider>
+      </SocketProvider>
     </BrowserRouter>
 );
--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './components/app';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-const socket = require('socket.io-client')(process.env.REACT_APP_API_URL);
-
-console.log(process.env);
-
-socket.on('connect', () => {
-  console.log('Cliente conectou!');
-});
-
 injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
