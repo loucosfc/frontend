63e90b35c4bd01bdbf4ba085088fe00ecfc1175d
lhas <luizhrqas@gmail.com>
2017-08-14 23:36:37 -0300

    fixed webpack error

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -89,6 +89,7 @@ module.exports = {
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
       'components': path.resolve(paths.appSrc, 'components/'),
+      'containers': path.resolve(paths.appSrc, 'containers/'),
       'services': path.resolve(paths.appSrc, 'services/'),
       'scenes': path.resolve(paths.appSrc, 'scenes/'),
     },
